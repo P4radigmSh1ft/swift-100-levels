3eda5a32dd4c33fe78e84f2d79d27a28c6ff8b0c
P4radigmSh1ft <hugo.sadaune@protonmail.com>
2025-07-14 19:02:43 +0200

    Delete Level-01/level_01.swift

--- Level-01/level_01.swift
+++
@@ -1,7 +0,0 @@
-let name: String = "Hugo"
-var age: Int = 29
-let goal: String = "Maîtriser Swift en 100 projets !"
-
-print("Bonjour, je m'appelle \(name) et j'ai \(age) ans.")
-print("Mon objectif : \(goal)")
-print("L'année prochaine, j'aurai \(age + 1) ans.")
