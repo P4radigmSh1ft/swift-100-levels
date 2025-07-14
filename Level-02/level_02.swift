b6f1d5e525f0024c6ea1b0788d3b13eeffa1d8ef
P4radigmSh1ft <hugo.sadaune@protonmail.com>
2025-07-14 19:07:30 +0200

    Add level 02

---
+++ Level-02/level_02.swift
@@ -0,0 +1,7 @@
+let name: String = "Hugo"
+var age: Int = 29
+let goal: String = "Master Swift in 100 projects!"
+
+print("Hi, my name is \(name) and I am \(age) years old.")
+print("My goal: \(goal)")
+print("Next year, I will be \(age + 1) years old.")
